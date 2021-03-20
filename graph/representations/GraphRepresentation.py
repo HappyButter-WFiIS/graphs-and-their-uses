4e363db2ad600e226573d452279c84000fb6764e
HappyButter <mikolaj.marchewa@gmail.com>
2021-03-20 22:37:37 +0100

    count_edges method

--- graph/representations/GraphRepresentation.py
+++ graph/representations/GraphRepresentation.py
@@ -2,9 +2,9 @@ from enum import Enum
 
 class RepresentationType(Enum):
     EMPTY = 0
-    ADJ_MAT = 1
-    ADJ_LIST = 2
-    INC_MAT = 3
+    ADJACENCY_MATRIX = 1
+    ADJACENCY_LIST = 2
+    INCIDENCE_MATRIX = 3
 
 class GraphRepresentation:
 
@@ -15,29 +15,22 @@ class GraphRepresentation:
     def __str__(self):
         result = ''
         for index, row in enumerate(self.repr):
-            if self.repr_type == RepresentationType.ADJ_LIST:
+            if self.repr_type == RepresentationType.ADJACENCY_LIST:
                 result += str(index + 1) + '. '
             for elem in row:
                 result += str(elem) + ' '
             result += '\n'
         return result
+
+
     """
     Creating various representations from file
     """
-    def create_adjacency_matrix(self, filename: str):
-        self.repr_type = RepresentationType.ADJ_MAT
-        self.__create(self.__read_file(filename))
-        print(self)
-    
-    def create_adjacency_list(self, filename: str):
-        self.repr_type = RepresentationType.ADJ_LIST
+    def create_representation(self, filename: str, representation_type: RepresentationType):
+        self.repr_type = representation_type
         self.__create(self.__read_file(filename))
         print(self)
 
-    def create_incidence_matrix(self, filename: str):
-        self.repr_type = RepresentationType.INC_MAT
-        self.__create(self.__read_file(filename))
-        print(self)
 
     def __read_file(self, filename: str): 
         try:
@@ -47,39 +40,46 @@ class GraphRepresentation:
             print("Wrong filename")
             return ''
 
+
     def __create(self, content: str):
         self.repr.clear()
         for line in content:
             row = list()
             line = line.split(' ')
-            if self.repr_type == RepresentationType.ADJ_LIST:
+            if self.repr_type == RepresentationType.ADJACENCY_LIST:
                 line = line[1:]
             for elem in line:
                 row.append(int(elem))
             self.repr.append(row)
+
+
     """
     Transform representations
     """
     def to_adjacency_matrix(self):
-        if self.repr_type == RepresentationType.ADJ_LIST:
+        if self.repr_type == RepresentationType.ADJACENCY_LIST:
             self.__from_adjlist_to_adjmat()
-        elif self.repr_type == RepresentationType.INC_MAT:
+        elif self.repr_type == RepresentationType.INCIDENCE_MATRIX:
             self.__from_incmat_to_adjmat()
         print(self)
 
+
     def to_adjacency_list(self):
-        if self.repr_type == RepresentationType.ADJ_MAT:
+        if self.repr_type == RepresentationType.ADJACENCY_MATRIX:
             self.__from_adjmat_to_adjlist()
-        elif self.repr_type == RepresentationType.INC_MAT:
+        elif self.repr_type == RepresentationType.INCIDENCE_MATRIX:
             self.__from_incmat_to_adjlist()
         print(self)
+
     
     def to_incidence_matrix(self):
-        if self.repr_type == RepresentationType.ADJ_LIST:
+        if self.repr_type == RepresentationType.ADJACENCY_LIST:
             self.__from_adjlist_to_incmat()
-        elif self.repr_type == RepresentationType.ADJ_MAT:
+        elif self.repr_type == RepresentationType.ADJACENCY_MATRIX:
             self.__from_adjmat_to_incmat()
         print(self)
+
+
     """
     To Adjacency Matrix
     """
@@ -88,6 +88,8 @@ class GraphRepresentation:
     
     def __from_incmat_to_adjmat(self):
         pass
+
+
     """
     To Adjacency List
     """
@@ -96,6 +98,8 @@ class GraphRepresentation:
 
     def __from_incmat_to_adjlist(self):
         pass
+
+
     """
     To Incidence Matrix
     """
@@ -104,7 +108,26 @@ class GraphRepresentation:
 
     def __from_adjlist_to_incmat(self):
         pass
+    
+    
+    """
+    Helper methods
+    """
+    def count_edges(self) -> int:
+        sum_edges = 0
 
-
-
-
+        if self.repr_type == RepresentationType.ADJACENCY_LIST:
+            for line in self.repr:
+                sum_edges += len(line)
+            sum_edges = sum_edges/2
+            
+        elif self.repr_type == RepresentationType.ADJACENCY_MATRIX:
+            for line in self.repr:
+                sum_edges += sum(line)
+            sum_edges = sum_edges/2
+        
+        elif self.repr_type == RepresentationType.INCIDENCE_MATRIX:
+            sum_edges = len(self.repr[0])
+       
+        return int(sum_edges)
+    
--- lab01/lab01.py
+++ lab01/lab01.py
@@ -2,17 +2,12 @@ import os, sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
-from graph.representations.GraphRepresentation import GraphRepresentation
+from graph.representations.GraphRepresentation import GraphRepresentation, RepresentationType
 
 if __name__ == "__main__":
     G = GraphRepresentation()
-    G.create_adjacency_matrix(os.path.dirname(__file__) + '/files/adjmat.txt')
-    G.create_incidence_matrix(os.path.dirname(__file__) + '/files/incmat.txt')
-    G.create_adjacency_list(os.path.dirname(__file__) + '/files/adjlist.txt')
+    G.create_representation(os.path.dirname(__file__) + '/files/adjmat.txt', RepresentationType.ADJACENCY_MATRIX)
+    print(G.count_edges())
 
-    
-    
-    
-
-
-        
+    # G.create_representation(os.path.dirname(__file__) + '/files/incmat.txt', RepresentationType.INCIDENCE_MATRIX)
+    # G.create_representation(os.path.dirname(__file__) + '/files/adjlist.txt', RepresentationType.ADJACENCY_LIST)
