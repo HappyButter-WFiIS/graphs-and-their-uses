04abad5f5ac81df1a377a50a538c8f9880adafe8
HubertWdowiak <hubert.wdowiak98@onet.pl>
2021-03-21 18:31:17 +0100

    small refactoring

--- lab01/utils.py
+++ lab01/utils.py
@@ -1,9 +1,14 @@
 import numpy as np
 
 
-def get_graph_with_probability(n, probability):
-    graph = np.random.random((n, n))
-    graph = graph >= probability
-    for i in range(n):
+def get_graph_with_probability(num_of_nodes: int, probability: float) -> np.ndarray:
+    graph = np.random.random((num_of_nodes, num_of_nodes))
+    graph = graph >= 1 - probability
+
+    for i in range(num_of_nodes):
         graph[i][i] = False
     return graph
+
+
+if __name__ == '__main__':
+    print(get_graph_with_probability(10, 1))
