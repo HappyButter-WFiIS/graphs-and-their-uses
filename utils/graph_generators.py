449ede07a8cb3ef73d9fbdd37079f627875e6d34
HappyButter <mikolaj.marchewa@gmail.com>
2021-03-21 18:49:18 +0100

    merge

--- utils/graph_generators.py
+++ utils/graph_generators.py
@@ -22,10 +22,10 @@ def my_function(numOfVertices, numOfEdges):
     
 # print(np.matrix(matrix))
 
-def get_graph_with_probability(n, probability):
-    graph = np.random.random((n, n))
+def get_graph_with_probability(num_of_nodes: int, probability: float) -> np.ndarray:
+    graph = np.random.random((num_of_nodes, num_of_nodes))
     graph = graph >= 1 - probability
-    
-    for i in range(n):
+
+    for i in range(num_of_nodes):
         graph[i][i] = False
     return graph
