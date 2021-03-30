a756b14b1be3f6b83e216d89405623280658a9ea
HubertWdowiak <hubert.wdowiak98@onet.pl>
2021-03-30 12:34:39 +0200

    randomized graph, raw version - need to optimize

---
+++ lab02/lab02_2.py
@@ -0,0 +1,43 @@
+import os, sys
+import numpy as np
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+from utils.GraphRepresentation import GraphRepresentation, RepresentationType
+from utils.GraphPlotter import plot_graph
+
+G = GraphRepresentation()
+G.load_data([4, 2, 2, 3, 2, 1, 4, 2, 2, 2, 2], RepresentationType.GRAPH_SEQUENCE)
+
+G.to_adjacency_list()
+G.to_adjacency_matrix()
+
+
+def randomize(G):
+    edges = []
+    for i in range(len(G.repr)):
+        for j in range(i):
+            if G.repr[i][j]:
+                edges.append((i, j))
+
+    first_edge = edges[np.random.randint(len(edges))]
+    second_edge = edges[np.random.randint(len(edges))]
+
+    a, b = first_edge
+    c, d = second_edge
+
+    if G.repr[a][d] or G.repr[c][b] or a == d or b == c:
+        randomize(G)
+    else:
+        G.repr[a][b] = G.repr[b][a] = 0
+        G.repr[c][d] = G.repr[d][c] = 0
+        G.repr[a][d] = G.repr[d][a] = 1
+        G.repr[c][b] = G.repr[b][c] = 1
+
+
+plot_graph(G.repr)
+for _ in range(1000):
+    randomize(G)
+plot_graph(G.repr)
