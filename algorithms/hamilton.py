b0d5965805319905572a18c99f33f2c79cc89e81
Arek <bombarder1118@o2.pl>
2021-05-11 11:07:30 +0200

    refactor - adding comments to hamilton algorithm

--- algorithms/hamilton.py
+++ algorithms/hamilton.py
@@ -1,6 +1,14 @@
 from array import *
 
-def isHamiltonianPathFromVertex(G, visited, curr_vertex) -> int:
+def isHamiltonianPathFromVertex(G: list, visited: list, curr_vertex: int) -> int:
+	"""
+    Check if exists hamiltonian path starting from curr_vertex
+	Returns 1 if hamiltonian path from curr_vertex exists, otherwise 0 
+    Parameters: 
+        G - representation of graph as adjecency list
+        visited - list that keeps info about already visited nodes
+        curr_vertex - starting vertex
+	"""
     visited[curr_vertex] = 1
     if not 0 in visited:
         return 1
@@ -9,8 +17,14 @@ def isHamiltonianPathFromVertex(G, visited, curr_vertex) -> int:
             if isHamiltonianPathFromVertex(G,visited,v): return 1
     visited[curr_vertex] = 0
     return 0
- 
-def isHamiltonian(G) -> int:
+
+def isHamiltonian(G: list) -> int:
+	"""
+    Check if exists hamiltonian path in graph G
+	Returns 1 if hamiltonian path exists, otherwise 0 
+    Parameters: 
+        G - representation of graph as adjecency list
+	"""
     visited = [0] * len(G)
     for v in range(len(G)):
         if(isHamiltonianPathFromVertex(G, visited, v) ):
