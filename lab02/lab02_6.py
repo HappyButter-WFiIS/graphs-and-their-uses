4d217c9c666f543fe2dd0f0258528e649349593e
arek <skiba.arkadius@gmail.com>
2021-05-12 09:02:55 +0200

    changing indexes to start from 0

--- lab02/lab02_6.py
+++ lab02/lab02_6.py
@@ -7,8 +7,8 @@ sys.path.append(parentdir)
 from algorithms.hamilton import isHamiltonian
 
 if __name__ == '__main__':
-    graph1 = [[2,4,5],[8,3,1],[4,7,2],[1,6,3],[1,8,6],[5,7,4],[8,6,3],[2,5,7]]  # adjacency list
-    graph2 = [[2,3,4,5,6], [1], [1], [1,5,6], [1,4,6], [1,4,5]]                 # adjacency list
+    graph1 = [[1,3,4],[7,2,0],[3,6,1],[0,5,2],[0,7,5],[4,6,3],[7,5,2],[1,4,6]]  # adjacency list
+    graph2 = [[1,2,3,4,5], [0], [0], [0,4,5], [0,3,5], [0,3,4]]                 # adjacency list
     
     print(isHamiltonian(graph1))  # output 1 means graph is hamiltonian
     print(isHamiltonian(graph2))  # output 0 means graph is not hamiltonian
