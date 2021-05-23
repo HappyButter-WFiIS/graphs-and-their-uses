20d2df91becd0be3839152217648324a7d04a442
Jan Zajda <j.zajda47@gmail.com>
2021-05-23 21:45:07 +0200

    kosaraju update

--- lab04/lab04_02.py
+++ lab04/lab04_02.py
@@ -4,32 +4,14 @@ currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
-from utils.Graph import Graph, RepresentationType
+from utils.DirectedGraph import DirectedGraph, RepresentationType
 from utils.graph_plotter import GraphPlotter
+from utils.graph_generators import get_directed_graph_with_probability
 from algorithms.kosaraju import kosaraju
 
 if __name__ == '__main__':
-    G = Graph()
-    G.load_data([[0,1,0,0,1,0,0,0,0],
-                 [0,0,1,0,0,0,0,0,0],
-                 [0,0,0,0,0,1,0,0,0],
-                 [0,0,0,0,0,0,0,0,0],
-                 [0,1,0,1,0,1,1,1,0],
-                 [0,0,0,0,0,0,0,0,1],
-                 [0,0,0,0,1,0,0,0,0],
-                 [0,0,0,0,0,0,1,0,0],
-                 [0,0,0,0,1,0,0,1,0]], RepresentationType.ADJACENCY_MATRIX) #example
-    
-    groups = kosaraju(G)
-    GraphPlotter.plot_graph(G, groups)
-    print('')
-    
-    G.load_data([[0, 1, 1, 0, 1, 0, 0],
-                 [1, 0, 1, 1, 1, 0, 1],
-                 [0, 0, 0, 0, 0, 1, 0],
-                 [0, 1, 0, 0, 0, 0, 1],
-                 [0, 0, 0, 0, 0, 0, 1],
-                 [0, 1, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 1, 0]], RepresentationType.ADJACENCY_MATRIX)
+    G = DirectedGraph()
+
+    G.load_data(get_directed_graph_with_probability(10, 0.2, 1, 1), RepresentationType.ADJACENCY_MATRIX)
     groups = kosaraju(G)
     GraphPlotter.plot_graph(G, groups)
